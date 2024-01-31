9e1d4ccd0f48b0a1bb67161d01338435b9cc3ac3
Jack Philippi <jackphilippi@gmail.com>
2024-01-31 20:05:55 +1100

    Added less invasive button

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-browser.storage.sync.get({ enable_modal: true }).then(items => {
+browser.storage.sync.get({ enable_modal: true, simple_button: true }).then(items => {
     // If it's a media URL, don't bother. See https://github.com/jackphilippi/old-reddit-redirect/issues/1
     if (window.location.href.includes("i.redd.it")) {
         return;
@@ -11,14 +11,26 @@ browser.storage.sync.get({ enable_modal: true }).then(items => {
     // Make sure we don't load multiple modals
     if (!document.getElementById("popup-content")) {
         var popup = document.createElement('div');
-        popup.innerHTML = `
-            <div id="popup-content" style="position: fixed; bottom: 10px; right: 10px; background-color: #f9f9f9; border: 1px solid #ddd; padding: 15px; z-index: 1000;">
-                <p style="margin-bottom: 10px;">Would you like to redirect to old reddit?</p>
-                <button id="redirect-btn" style="background-color: #4285f4; color: white; border: none; padding: 10px 15px; cursor: pointer; right: 0;">Go to old.reddit.com</button>
-                <button id="close-btn" style="background-color: #666666; color: white; border: none; padding: 10px 15px; cursor: pointer; right: 0;">Close</button>
-                <p style="font-size: 8px; margin-top: 10px; text-align: right; width: 100%;">old reddit viewer by fippi</p>
-            </div>
-        `;
+
+        if (!items.simple_button) {
+            popup.innerHTML = `
+                <div id="popup-content" style="position: fixed; bottom: 10px; right: 10px; background-color: #f9f9f9; border: 1px solid #ddd; padding: 15px; z-index: 1000;">
+                    <p style="margin-bottom: 10px;">Would you like to redirect to old reddit?</p>
+                    <button id="redirect-btn" style="height: auto; background-color: #4285f4; padding-left: 10px; padding-right: 10px; color: white; border: none; cursor: pointer;">Go to old.reddit.com</button>
+                    <button id="close-btn" style="height: auto; background-color: #666666; padding-left: 10px; padding-right: 10px; color: white; border: none; cursor: pointer;">Close</button>
+                    <p style="font-size: 8px; margin-top: 10px; text-align: right; width: 100%;">old reddit viewer by fippi</p>
+                </div>
+            `;
+        } else {
+            popup.innerHTML = `
+                <style>
+                    a#redirect-btn:hover { text-shadow: 1px 1px #f0a008 !important; }
+                </style>
+                <div id="popup-content" style="position: fixed; bottom: 10px; right: 10px; background-color: #f9f9f9; border: 0; padding: 0; background: transparent; z-index: 1000;">
+                    <a id="redirect-btn" style="text-decoration: none;cursor: pointer; font-size: 2em;">🥕</a>
+                </div>
+            `;
+        }
 
         // Add popup to DOM
         document.body.appendChild(popup);
--- options.js
+++ options.js
@@ -1,17 +1,21 @@
 // Saves options to browser.storage
 function saveOptions() {
     var enable_modal = document.getElementById('enable_modal').checked;
+    var simple_button = document.getElementById('simple_button').checked;
     browser.storage.sync.set({
-        enable_modal: enable_modal
+        enable_modal,
+        simple_button
     });
 }
 
 // Restores the preferences stored in browser.storage
 function restoreOptions() {
-    browser.storage.sync.get({ enable_modal: true }, function(items) {
+    browser.storage.sync.get({ enable_modal: true, simple_button: true }, function(items) {
         document.getElementById('enable_modal').checked = items.enable_modal;
+        document.getElementById('simple_button').checked = items.simple_button;
     });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('enable_modal').addEventListener('change', saveOptions);
+document.getElementById('simple_button').addEventListener('change', saveOptions);
